9775424a7c0b578393c920bfcb36055867cbf60a
tatpongkatanyukul <32717432+tatpongkatanyukul@users.noreply.github.com>
2024-02-23 00:27:30 +0700

    Add files via upload

---
+++ Phase1/AutoBridgeV3.py
@@ -0,0 +1,177 @@
+"""
+Front-End: open in Blender
+Derived from 
+* D230201/AutoPlateV10.py
+* D230203/AutoBridgeV1.py (2024/02/10)
+* D240218/AutoBridgeV2.py (2024/02/22)
+
+Refactoring
+"""
+
+print('\n' + '-'*80)
+print('# Front-End script')
+
+import time
+print('    Current time:', time.ctime())
+   
+
+import bpy
+
+import os
+s_cwd = os.getcwd()
+print('    Current working directory =', s_cwd)
+
+
+# Locate working path
+import pathlib
+
+script_path = __file__
+print(f"    Script file = {__file__}")
+
+# Check if we are running from the Blender Text Editor
+if bpy.context.space_data != None and bpy.context.space_data.type == 'TEXT_EDITOR':
+    # Get the path to the SAVED script when running from Blender
+    script_path = bpy.context.space_data.text.filepath
+#    print('   * bpy.context.space_data.text.filepath =', script_path)
+
+script_dir = pathlib.Path(script_path).resolve().parent
+#print(f" * script directory = {script_dir}")
+
+work_path = str(script_dir)
+#work_path = r"C:\Y2024\iDNAM\D230203"
+
+###################
+## Read setup.json
+###################
+
+import json
+
+fsetup='setup.json' 
+psetup = os.path.join(work_path,fsetup)
+
+# Read configuration and parameters
+setup_vars = {} 
+
+with open(psetup) as f:
+   setup_vars = json.load(f)
+
+####################
+# Add working path
+####################
+
+import sys
+sys.path.append(work_path)
+
+print("    * Append working path:", work_path)
+
+####################
+# Add scipy path
+####################
+
+# Path for scipy
+packages_path = setup_vars["scipy_path"]
+sys.path.insert(0, packages_path )
+
+print("    * Set-up scipy path:", packages_path)
+
+#print("      Working path content:")
+#print('      * ', os.listdir(work_path))
+
+import DTPanelV6 as dt              # GUI
+import BUtilsV6 as uu               # General blender utils
+#import RidgeIdenV? as ri           # Ridge identification
+#import ShapeApproxV? as sa         # Shape approximation
+#import CrossSecApproxV? as csa     # Cross-section approximation
+
+import importlib
+importlib.reload(dt)
+importlib.reload(uu)
+#importlib.reload(ri)
+#importlib.reload(sa)
+#importlib.reload(csa)
+
+
+
+#############################
+# BEGIN MAIN
+#############################    
+
+
+if __name__ == '__main__':
+
+
+   print('\n# Main')
+
+   cfg='conf.json' 
+   pcfg = os.path.join(work_path, cfg)
+   print(f'  * Read cfg: {pcfg}')
+   
+
+   # Read configuration and parameters
+   pvars = {} 
+
+   with open(pcfg) as f:
+      # Read pvars from cfg
+      pvars = json.load(f)
+      assert len(pvars) > 0, f"Configuration file ({pcfg}) error"
+
+   logfile = os.path.join(work_path, pvars['log'])
+
+   # Clear log
+   uu.clear_log(logfile)
+
+   # Start a new log
+   uu.log(f'AutoBridge: Read configuration {cfg}', logfile)
+
+
+   # Set-up DT Panel 
+   print(f'  * Set-up DT Panel')
+    
+   dt.wpath=work_path 
+   
+   # Aj Wasu's params
+   dt.vert_double_threshold = 0.1   # per 3D printer capability
+   dt.dissolve_threshold = 0.01
+   
+   # TK's params
+   dt.config_file = pcfg
+   dt.pvars = pvars
+   dt.logfile = logfile
+
+   dt.register()
+#   dt.unregister()
+
+   uu.log('AutoBridge: Set-up DT panel', logfile)
+
+
+   # Set-up object names   
+   dt.CODE = {"T": "T1", "Tp": "T1'", "C": "C1",
+              "RO": "RO", "RI": "RI",
+              "target": "SCAN"} 
+
+   print(f'  * Set-up object names:', dt.CODE)
+   
+   # Set-up materials
+   dmats = pvars["materials"]
+      
+      
+   # Clear materials        
+   uu.clear_materials(dt.CODE, clear_target="TARGET")
+
+   # Add materials      
+   dt.mat_dict = uu.add_materials(dmats)
+
+   uu.log('AutoBridge: Set-up object names', logfile)
+
+
+   print('  * DT Status =', dt.status)
+
+   print('\n# End Main')
+   print('-'*80 + '\n')
+   
+   
+
+#############################
+# END MAIN
+#############################    
+
---
+++ Phase1/CrossSecApproxV7.py
@@ -0,0 +1,159 @@
+import numpy as np
+
+def approx_xsect(faces_rtz, shape_obj, bound_params, resolution):
+    '''
+    xsect_verts, fb = approx_xsect(faces, 
+        {"name": shape_name, "obj": dshape}, 
+        {"angle_keys": ["T1", "C1"], "radial_bounds": ["RO", "RI"], "reso": 40})    
+    '''
+
+    # import pickle
+    # with open("C:\\Users\\tatpong\\Desktop\\2024\\Phase1\\Debug_Data.pkl", 'wb') as file: 
+    #     pickle.dump((faces_rtz, shape_obj, bound_params, 
+    #              remov_outlier, resolution), file)
+
+
+    print(f"CrossSecApprox.approx_xsect: faces {faces_rtz.shape}, shape {shape_obj}, bounds {bound_params}, resolution {resolution}")
+
+    fb = {}
+
+    phi1, phi2 = bound_params["angles"]
+
+    ell_rs = shape_obj.calc_radius(faces_rtz[:,1])
+
+    rmarks = None
+    remov_outlier = False
+    if bound_params["radial_mode"] == "Z":
+    
+        z1, z2 = bound_params["zs"] # (inner, outer)
+
+        inners = (faces_rtz[:,0] < ell_rs) & (faces_rtz[:,2] >= z1)
+
+        outers = (faces_rtz[:,0] >= ell_rs) & (faces_rtz[:,2] >= z2)
+
+        rmarks = inners | outers
+
+        remov_outlier = True
+
+    elif bound_params["radial_mode"] == "R":
+
+        dr1, dr2 = bound_params['drs']
+
+        rmarks = (faces_rtz[:,0] >= ell_rs + dr1) & \
+                 (faces_rtz[:,0] <= ell_rs + dr2)
+
+    else:
+        raise Exception(f"Error: radial_mode = {bound_params['radial_mode']}")
+
+    tmarks = (faces_rtz[:,1] >= phi1) & \
+             (faces_rtz[:,1] <= phi2)
+
+    ids_xs = np.where(rmarks & tmarks)[0]
+    segment = faces_rtz[ids_xs, :]
+    fb["coverage_ids"] = ids_xs
+    fb["eff_ids"] = ids_xs
+    print(f"    * Coverage: {len(ids_xs)} faces")
+
+    # segment = faces_rtz[rmarks & tmarks, :]
+
+    # 1.4. Normalize r per elliptical center
+    thetas = segment[:,1]
+    ell_rs = shape_obj.calc_radius(thetas)
+    norm_segment = segment  # Alias
+    norm_segment[:,0] = segment[:,0] - ell_rs
+
+    # 1.5. Remove outliers: r that is too large or too small
+    if remov_outlier:
+        print(f"    * Remove outliers")
+        # 1.5.1. Determine Q1, Q3, and iQR
+        Qs = [0, 0.25, 0.5, 0.75, 1]
+        rQ = {}
+        for i, q in enumerate(Qs):
+            rQ[i] = np.quantile(norm_segment[:,0], q)
+            print('      Q {:.2f}: x = {:.2f}'.format(q, rQ[i]) )
+
+        iQR = rQ[3] - rQ[1]
+        #print('iQR =', iQR)
+
+        tau = [rQ[1] - 1.5*iQR, rQ[3] + 1.5*iQR]
+
+        # 1.5.2. Keep only points whose tau[0] < r < tau[1]
+        valid_marks = (norm_segment[:,0] > tau[0]) & \
+                        (norm_segment[:,0] < tau[1])
+        norm_segment = norm_segment[valid_marks, :]
+
+        fb["eff_ids"] = ids_xs[valid_marks]
+    # end if
+
+    rpmin = np.min(norm_segment[:, 0])
+    rpmax = np.max(norm_segment[:, 0])
+
+    tpmin = np.min(norm_segment[:, 1])
+    tpmax = np.max(norm_segment[:, 1])
+
+    zpmin = np.min(norm_segment[:, 2])
+    zpmax = np.max(norm_segment[:, 2])
+
+    print(f"    * 1. Polar data is ready: r in [{rpmin:.3f}, {rpmax:.3f}],", end=" ")
+    print(f"theta in [{tpmin:.3f}, {tpmax:.3f}],", end=" ")
+    print(f"z in [{zpmin:.3f}, {zpmax:.3f}]; shape = {norm_segment.shape}")
+
+    # 2. Approximate the cross-section outline    
+    normrz = norm_segment[:, [0,2]]
+
+    # 2.1. Discretize the r-z datapoints into bins
+    # Work on r-z projection
+    
+    rs = np.linspace(rpmin, rpmax, resolution)
+
+    bins = []
+    # Go through each bin
+    zpmax = zpmin
+    rpmin = rs[0]
+    for i in range(resolution-1):
+        rpmax = rs[i+1]
+
+        # Coverage: 2 bins
+        mark = (normrz[:,0] >= rpmin) & (normrz[:,0] < rpmax)
+        # print(i, f"[{rpmin} , {rpmax}]: sum(mark) =", np.sum(mark))
+
+        if np.sum(mark) > 0:
+            zpmax = np.max(normrz[mark, 1])
+
+        rpi = rs[i]
+        bins.append([rpi, zpmax])
+
+        # The next height counts from the previous bin to its bin.
+        rpmin = rpi
+
+    # end for i
+    
+    # The last one covers only one last bin.
+    zlast = zpmax
+    mark = (normrz[:,0] >= rs[-2]) & (normrz[:,0] <= rs[-1])
+    if np.sum(mark) > 0:
+        zlast = np.max(normrz[mark, 1])
+
+    bins.append([rpmax, zlast])
+
+    bins = np.array(bins)
+
+    # # 2.2. Correct the peak and the trailing edge
+    # #      so that no point lies above its bin.
+    # id_bin_zmax = np.argmax(bins[:,1])
+    # bin_zmax = bins[id_bin_zmax,1]
+
+    # # Outer side uses the trailing edge.
+    # backrows = np.c_[bins[(id_bin_zmax+1):, 0], 
+    #                  bins[id_bin_zmax:-1, 1]]
+
+    # corrected_bins = np.r_[bins[:(id_bin_zmax+1),:], backrows] # (N,2) of [r,z]
+
+    # [r, z] of shape (N, 2).
+    # Make vertices in [r, z, 0]
+    N, _ = bins.shape
+    verts = np.c_[bins, np.zeros((N,1))]   # (N,3) of [r, z, 0] for x,y,z coordinates
+
+    print(f"    * 2. Discretized cross-section is ready: {verts.shape}")
+    print("CrossSecApprox.approx_xsect: done.")
+    return verts, fb
